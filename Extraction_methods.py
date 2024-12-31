2ccc83bf3a4dc25ee62654e7e266d7be4dc55e17
gabriele rizzitiello <frizzy2001@gmail.com>
2024-12-31 17:04:51 +0100

    Update relational extraction
    testing relational tables

--- Extraction_methods.py
+++ Extraction_methods.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 
+# Esempio di utilizzo
+html_content = '''
+'''
 def extract_claims_from_relational_table(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     table = soup.find('table')
@@ -10,11 +13,16 @@ def extract_claims_from_relational_table(html_content):
         
         # Estrai le intestazioni
         col_headers = [th.get_text(strip=True) for th in rows[0].find_all('th')]
-        
+        col_headers_td = [td.get_text(strip=True) for td in rows[0].find_all('td')]
+        sum_col_headers = len(col_headers) + len(col_headers_td)
+        if len(col_headers_td) != 0:
+            col_headers.extend(col_headers_td)
+
         # Itera sulle righe, escludendo la riga di intestazione
         for row in rows[1:]:
             cells = row.find_all(['td', 'th'])  # Inclusi eventuali <th> nelle righe
-            if len(cells) != len(col_headers):
+            
+            if len(cells) != sum_col_headers:
                 continue  # Salta righe malformate
             
             # Combina intestazioni e celle
@@ -23,14 +31,9 @@ def extract_claims_from_relational_table(html_content):
 
         #extracted_data in input al LLM con LLM_Extraction_Prompt per generare i claims corretti
 
-
-    
     return extracted_data
 
-# Test del codice
-html_content = '''
-'''
-
-claims = extractclaims_from_relational_table(html_content)
+claims = extract_claims_from_relational_table(html_content)
 for claim in claims:
     print(claim)
+
